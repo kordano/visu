65a263f89bbf3a3af9bb085985958931dfedbd89
kordano <konrad.kuehne@rocketmail.com>
2013-12-01 18:03:41 +0100

    add wordcloud files readings

--- src/clj/visu/core.clj
+++ src/clj/visu/core.clj
@@ -1,8 +1,12 @@
 (ns visu.core
-  (:require [visu.exercise.cancer-analysis :as cancer :refer [clean-data]]))
+  (:require [clojure.string :refer [split]]
+            [visu.exercise.cancer-analysis :as cancer :refer [clean-data]]
+            [visu.parser :as parser :refer [get-text-frequencies]]))
 
 
 (defn get-data [name]
   (condp = name
     "cancer" (cancer/clean-data "data/cancer_data.csv")
+    "manifesto" (parser/get-text-frequencies "data/kommunistisches_manifest.txt")
+    "english_stopwords" (split (slurp "data/english_stopwords.txt") #",")
     "data not found"))
