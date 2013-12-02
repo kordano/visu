5964604bc0052a317a2453bfae3fe92d230839d4
kordano <konrad.kuehne@rocketmail.com>
2013-12-02 01:10:23 +0100

    add keyword removal

--- src/clj/visu/parser.clj
+++ src/clj/visu/parser.clj
@@ -4,6 +4,7 @@
             [clojure.string :refer [split replace blank? lower-case]]))
 
 (def special-characters [";" "," "\\." "!" "\\?" ":" "\\*" "\""  "'" "_" "-" "\\)" "\\(" "\r" "\\]" "\\["])
+(def stopwords (into #{} (split (slurp "data/english_stopwords.txt") #",")))
 
 (defn- remove-special-chars [word special-chars-list]
   (if-not (seq special-chars-list)
@@ -11,7 +12,10 @@
     (recur (replace word (re-pattern (first special-chars-list)) "") (rest special-chars-list))))
 
 (defn get-text-frequencies [path]
-  (let [lines (split (slurp "data/kommunistisches_manifest.txt") #"\n")
-        raw-words (flatten (map #(split % #" ") lines))
-        special-characters-removed (map #(remove-special-chars % special-characters) raw-words)]
-    (frequencies (remove blank? special-characters-removed))))
+  (->> (split (slurp path) #"\n")
+       (map #(split % #" "))
+       flatten
+       (map #(remove-special-chars % special-characters))
+       (remove blank?)
+       (remove stopwords)
+       frequencies))
