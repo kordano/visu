864aede87657a6eb9e502c3dcc8f4201a4dd1b35
kordano <konrad.kuehne@rocketmail.com>
2013-12-11 23:31:46 +0100

    make graph data available

--- src/clj/visu/core.clj
+++ src/clj/visu/core.clj
@@ -1,11 +1,12 @@
 (ns visu.core
   (:require [clojure.string :refer [split]]
             [visu.exercise.cancer-analysis :as cancer :refer [clean-data]]
-            [visu.parser :as parser :refer [get-text-frequencies]]))
+            [visu.parser :as parser :refer [get-text-frequencies get-adjacency-list]]))
 
 
 (defn get-data [name]
   (case name
     "cancer" (cancer/clean-data "data/cancer_data.csv")
     "wordcloud" (parser/get-text-frequencies "data/kommunistisches_manifest.txt")
+    "graph" (parser/get-adjacency-list "data/graph.adjl")
     "data not found"))
