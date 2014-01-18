655c73b88d9fa407485e8f3e0efda353fc60430d
kordano <konrad.kuehne@rocketmail.com>
2014-01-18 16:53:54 +0100

    adjust to weatherdata

--- src/clj/visu/core.clj
+++ src/clj/visu/core.clj
@@ -1,7 +1,7 @@
 (ns visu.core
   (:require [clojure.string :refer [split]]
             [visu.exercise.cancer-analysis :as cancer :refer [clean-data]]
-            [visu.parser :as parser :refer [get-text-frequencies get-adjacency-list]]))
+            [visu.parser :as parser :refer [get-text-frequencies get-adjacency-list get-weather-data]]))
 
 
 (defn get-data [name]
@@ -9,4 +9,5 @@
     "cancer" (cancer/clean-data "data/cancer_data.csv")
     "wordcloud" (parser/get-text-frequencies "data/kommunistisches_manifest.txt")
     "graph" (parser/get-adjacency-list "data/graph.adjl")
+    "weatherdata" (parser/get-weather-data "data/niederschlag.vtk")
     "data not found"))
